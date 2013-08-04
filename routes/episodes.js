4c46a2b0cd7c17188487dd6756ae41bba50917fb
Jim <jim@amplience.com>
2013-08-04 19:00:58 +0100

    Express app for Pony Countdown API
    Signed-off-by: Jim <jim@amplience.com>

---
+++ routes/episodes.js
@@ -0,0 +1,78 @@
+var episodes = [
+    [],
+    [
+        {"name": "The Return of Harmony - Part 1", "time": "2011-09-17T12:00:00.000Z", "season": 2, "episode": 1, "duration": 20},
+        {"name": "The Return of Harmony - Part 2", "time": "2011-09-24T13:00:00.000Z", "season": 2, "episode": 2, "duration": 20},
+        {"name": "Lesson Zero", "time": "2011-10-15T13:00:00.000Z", "season": 2, "episode": 3, "duration": 20},
+        {"name": "Luna Eclipsed", "time": "2011-10-22T13:00:00.000Z", "season": 2, "episode": 4, "duration": 20},
+        {"name": "Sisterhooves Social", "time": "2011-11-05T14:00:00.000Z", "season": 2, "episode": 5, "duration": 20},
+        {"name": "The Cutie Pox", "time": "2011-11-12T15:00:00.000Z", "season": 2, "episode": 6, "duration": 20},
+        {"name": "May the Best Pet Win!", "time": "2011-11-19T15:00:00.000Z", "season": 2, "episode": 7, "duration": 20},
+        {"name": "The Mysterious Mare Do Well", "time": "2011-11-26T15:00:00.000Z", "season": 2, "episode": 8, "duration": 20},
+        {"name": "Sweet and Elite", "time": "2011-12-03T15:00:00.000Z", "season": 2, "episode": 9, "duration": 20},
+        {"name": "Secret of My Excess", "time": "2011-12-10T15:00:00.000Z", "season": 2, "episode": 10, "duration": 20},
+        {"name": "Hearth's Warming Eve", "time": "2011-12-17T15:00:00.000Z", "season": 2, "episode": 11, "duration": 20},
+        {"name": "Family Appreciation Day", "time": "2012-01-07T15:00:00.000Z", "season": 2, "episode": 12, "duration": 20},
+        {"name": "Baby Cakes", "time": "2012-01-14T15:00:00.000Z", "season": 2, "episode": 13, "duration": 20},
+        {"name": "The Last Roundup", "time": "2012-01-21T15:00:00.000Z", "season": 2, "episode": 14, "duration": 20},
+        {"name": "The Super Speedy Cider Squeezy 6000", "time": "2012-01-28T15:00:00.000Z", "season": 2, "episode": 15, "duration": 20},
+        {"name": "Read It and Weep", "time": "2012-02-04T15:00:00.000Z", "season": 2, "episode": 16, "duration": 20},
+        {"name": "Hearts and Hooves Day", "time": "2012-02-11T15:30:00.000Z", "season": 2, "episode": 17, "duration": 20},
+        {"name": "A Friend in Deed", "time": "2012-02-18T15:00:00.000Z", "season": 2, "episode": 18, "duration": 20},
+        {"name": "Putting Your Hoof Down", "time": "2012-03-03T18:00:00.000Z", "season": 2, "episode": 19, "duration": 20},
+        {"name": "It's About Time", "time": "2012-03-10T18:00:00.000Z", "season": 2, "episode": 20, "duration": 20},
+        {"name": "Dragon Quest", "time": "2012-03-17T17:00:00.000Z", "season": 2, "episode": 21, "duration": 20},
+        {"name": "Hurricane Fluttershy", "time": "2012-03-24T17:00:00.000Z", "season": 2, "episode": 22, "duration": 20},
+        {"name": "Ponyville Confidential", "time": "2012-03-31T16:00:00.000Z", "season": 2, "episode": 23, "duration": 20},
+        {"name": "MMMystery on the Friendship Express", "time": "2012-04-07T16:00:00.000Z", "season": 2, "episode": 24, "duration": 20},
+        {"name": "A Canterlot Wedding - Part 1", "time": "2012-04-21T16:00:00.000Z", "season": 2, "episode": 25, "duration": 20},
+        {"name": "A Canterlot Wedding - Part 2", "time": "2012-04-21T16:30:00.000Z", "season": 2, "episode": 26, "duration": 20}
+    ],
+    [
+        {"name": "The Crystal Empire - Part 1", "time": "2012-11-10T15:00:00.000Z", "season": 3, "episode": 1, "duration": 20},
+        {"name": "The Crystal Empire - Part 2", "time": "2012-11-10T15:30:00.000Z", "season": 3, "episode": 2, "duration": 20},
+        {"name": "Too Many Pinkie Pies", "time": "2012-11-17T15:30:00.000Z", "season": 3, "episode": 3, "duration": 20},
+        {"name": "One Bad Apple", "time": "2012-11-24T15:30:00.000Z", "season": 3, "episode": 4, "duration": 20},
+        {"name": "Magic Duel", "time": "2012-12-01T15:30:00.000Z", "season": 3, "episode": 5, "duration": 20},
+        {"name": "Sleepless in Ponyville", "time": "2012-12-08T15:30:00.000Z", "season": 3, "episode": 6, "duration": 20},
+        {"name": "Wonderbolt Academy", "time": "2012-12-15T15:30:00.000Z", "season": 3, "episode": 7, "duration": 20},
+        {"name": "Apple Family Reunion", "time": "2012-12-22T15:30:00.000Z", "season": 3, "episode": 8, "duration": 20},
+        {"name": "Spike at Your Service", "time": "2012-12-29T15:30:00.000Z", "season": 3, "episode": 9, "duration": 20},
+        {"name": "Keep Calm and Flutter On", "time": "2013-01-19T15:30:00.000Z", "season": 3, "episode": 10, "duration": 20},
+        {"name": "Just For Sidekicks", "time": "2013-01-26T15:30:00.000Z", "season": 3, "episode": 11, "duration": 20},
+        {"name": "Games Ponies Play", "time": "2013-02-09T15:30:00.000Z", "season": 3, "episode": 12, "duration": 20},
+        {"name": "Magical Mystery Cure", "time": "2013-02-16T15:30:00.000Z", "season": 3, "episode": 13, "duration": 20}
+    ],
+    [
+        {"name": "", "time": "2013-11-23T23:59:59.000Z", "season": 4, "episode": 1, "duration": 20}
+    ]
+];
+
+
+exports.all = function(req, res){
+    var merged = [];
+    res.send(merged.concat.apply(merged, episodes));
+};
+
+
+exports.season = function(req, res){
+    var season = episodes[req.params.season - 1];
+    res.send(season || []);
+};
+
+exports.episode = function(req, res){
+    var season = episodes[req.params.season - 1];
+    res.send(season ? season[req.params.episode - 1] : {});
+};
+
+exports.until = function(req, res){
+    var season = episodes[req.params.season - 1];
+    var episode = season ? season[req.params.episode - 1] : {};
+
+    if(episode.time){
+        res.send([new Date(episode.time) - new Date()]);
+    }else{
+        res.send(404);
+    }
+
+};
