eed45181bae9e272db0ad795f0d33571489ac208
Jim Allanson <jim@amplience.com>
2014-04-02 19:16:46 +0100

    Updated upcoming episodes to account for daylight savings. Should be correct this time...

--- episodes.js
+++ episodes.js
@@ -92,12 +92,12 @@ var episodes = [
         {"name": "Maud Pie", "time": "2014-03-15T14:30:00.000Z", "duration": 22},
         {"name": "For Whom the Sweetie Belle Toils", "time": "2014-03-22T14:30:00.000Z", "duration": 22},
         {"name": "Leap of Faith", "time": "2014-03-29T14:30:00.000Z", "duration": 22},
-        {"name": "Testing, Testing, 1, 2, 3", "time": "2014-04-05T14:30:00.000Z", "duration": 22},
-        {"name": "Trade Ya", "time": "2014-04-19T14:30:00.000Z", "duration": 22},
-        {"name": "Inspiration Manifestation", "time": "2014-04-26T14:30:00.000Z", "duration": 22},
-        {"name": "Equestria Games", "time": "2014-05-03T14:30:00.000Z", "duration": 22},
-        {"name": "", "time": "2014-05-10T14:30:00.000Z", "duration": 22},
-        {"name": "", "time": "2014-05-17T14:30:00.000Z", "duration": 22}
+        {"name": "Testing, Testing, 1, 2, 3", "time": "2014-04-05T15:30:00.000Z", "duration": 22},
+        {"name": "Trade Ya", "time": "2014-04-19T15:30:00.000Z", "duration": 22},
+        {"name": "Inspiration Manifestation", "time": "2014-04-26T15:30:00.000Z", "duration": 22},
+        {"name": "Equestria Games", "time": "2014-05-03T15:30:00.000Z", "duration": 22},
+        {"name": "", "time": "2014-05-10T15:30:00.000Z", "duration": 22},
+        {"name": "", "time": "2014-05-17T15:30:00.000Z", "duration": 22}
     ]
 ];
 
