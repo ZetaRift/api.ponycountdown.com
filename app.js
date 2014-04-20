3ea935d7816b88d73481ea1492218d8323e85f63
Jim Allanson <jim@amplience.com>
2014-04-20 22:16:45 +0100

    Added duration property automatically unless otherwise specified, tidied up autogenerated crap in app.js

--- app.js
+++ app.js
@@ -1,25 +1,8 @@
 var express = require('express')
   , episodes = require('./episodes')
-  , http = require('http')
-  , path = require('path');
+  , http = require('http');
 
 var app = express();
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 
 app.get('/until/:season/:episode', episodes.until);
 app.get('/until/next', episodes.untilNext);
@@ -28,7 +11,4 @@ app.get('/', episodes.all);
 app.get('/:season', episodes.season);
 app.get('/:season/:episode', episodes.episode);
 
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+http.createServer(app).listen(process.env.PORT || 3000);
--- episodes.js
+++ episodes.js
@@ -1,103 +1,103 @@
 var episodes = [
     [
-        {"name": "Friendship is Magic - Part 1", "time": "2010-10-10T12:00:00.000Z", "duration": 22},
-        {"name": "Friendship is Magic - Part 2", "time": "2010-10-22T12:00:00.000Z", "duration": 22},
-        {"name": "The Ticket Master", "time": "2010-10-29T12:00:00.000Z", "duration": 22},
-        {"name": "Applebuck Season", "time": "2010-11-05T12:00:00.000Z", "duration": 22},
-        {"name": "Griffon the Brush-Off", "time": "2010-11-12T12:00:00.000Z", "duration": 22},
-        {"name": "Boast Busters", "time": "2010-11-19T12:00:00.000Z", "duration": 22},
-        {"name": "Dragonshy", "time": "2010-11-26T12:00:00.000Z", "duration": 22},
-        {"name": "Look Before You Sleep", "time": "2010-12-03T12:00:00.000Z", "duration": 22},
-        {"name": "Bridle Gossip", "time": "2010-12-10T12:00:00.000Z", "duration": 22},
-        {"name": "Swarm of the Century", "time": "2010-12-17T12:00:00.000Z", "duration": 22},
-        {"name": "Winter Wrap Up", "time": "2010-12-24T12:00:00.000Z", "duration": 22},
-        {"name": "Call of the Cutie", "time": "2010-01-07T12:00:00.000Z", "duration": 22},
-        {"name": "Fall Weather Friends", "time": "2010-01-28T12:00:00.000Z", "duration": 22},
-        {"name": "Suited for Success", "time": "2010-02-04T12:00:00.000Z", "duration": 22},
-        {"name": "Feeling Pinkie Keen", "time": "2010-02-11T12:00:00.000Z", "duration": 22},
-        {"name": "Sonic Rainboom", "time": "2010-02-18T12:00:00.000Z", "duration": 22},
-        {"name": "Stare Master", "time": "2010-02-25T12:00:00.000Z", "duration": 22},
-        {"name": "The Show Stoppers", "time": "2010-03-04T12:00:00.000Z", "duration": 22},
-        {"name": "A Dog and Pony Show", "time": "2010-03-11T12:00:00.000Z", "duration": 22},
-        {"name": "Green Isn't Your Color", "time": "2010-03-18T12:00:00.000Z", "duration": 22},
-        {"name": "Over a Barrel", "time": "2010-03-25T12:00:00.000Z", "duration": 22},
-        {"name": "A Bird in the Hoof", "time": "2010-04-08T12:00:00.000Z", "duration": 22},
-        {"name": "The Cutie Mark Chronicles", "time": "2010-04-15T12:00:00.000Z", "duration": 22},
-        {"name": "Owl's Well that Ends Well", "time": "2010-04-22T12:00:00.000Z", "duration": 22},
-        {"name": "Party of One", "time": "2010-04-29T12:00:00.000Z", "duration": 22},
-        {"name": "The Best Night Ever", "time": "2010-05-06T12:00:00.000Z", "duration": 22}
+        {"name": "Friendship is Magic - Part 1", "time": "2010-10-10T12:00:00.000Z"},
+        {"name": "Friendship is Magic - Part 2", "time": "2010-10-22T12:00:00.000Z"},
+        {"name": "The Ticket Master", "time": "2010-10-29T12:00:00.000Z"},
+        {"name": "Applebuck Season", "time": "2010-11-05T12:00:00.000Z"},
+        {"name": "Griffon the Brush-Off", "time": "2010-11-12T12:00:00.000Z"},
+        {"name": "Boast Busters", "time": "2010-11-19T12:00:00.000Z"},
+        {"name": "Dragonshy", "time": "2010-11-26T12:00:00.000Z"},
+        {"name": "Look Before You Sleep", "time": "2010-12-03T12:00:00.000Z"},
+        {"name": "Bridle Gossip", "time": "2010-12-10T12:00:00.000Z"},
+        {"name": "Swarm of the Century", "time": "2010-12-17T12:00:00.000Z"},
+        {"name": "Winter Wrap Up", "time": "2010-12-24T12:00:00.000Z"},
+        {"name": "Call of the Cutie", "time": "2010-01-07T12:00:00.000Z"},
+        {"name": "Fall Weather Friends", "time": "2010-01-28T12:00:00.000Z"},
+        {"name": "Suited for Success", "time": "2010-02-04T12:00:00.000Z"},
+        {"name": "Feeling Pinkie Keen", "time": "2010-02-11T12:00:00.000Z"},
+        {"name": "Sonic Rainboom", "time": "2010-02-18T12:00:00.000Z"},
+        {"name": "Stare Master", "time": "2010-02-25T12:00:00.000Z"},
+        {"name": "The Show Stoppers", "time": "2010-03-04T12:00:00.000Z"},
+        {"name": "A Dog and Pony Show", "time": "2010-03-11T12:00:00.000Z"},
+        {"name": "Green Isn't Your Color", "time": "2010-03-18T12:00:00.000Z"},
+        {"name": "Over a Barrel", "time": "2010-03-25T12:00:00.000Z"},
+        {"name": "A Bird in the Hoof", "time": "2010-04-08T12:00:00.000Z"},
+        {"name": "The Cutie Mark Chronicles", "time": "2010-04-15T12:00:00.000Z"},
+        {"name": "Owl's Well that Ends Well", "time": "2010-04-22T12:00:00.000Z"},
+        {"name": "Party of One", "time": "2010-04-29T12:00:00.000Z"},
+        {"name": "The Best Night Ever", "time": "2010-05-06T12:00:00.000Z"}
 
     ],
     [
-        {"name": "The Return of Harmony - Part 1", "time": "2011-09-17T12:00:00.000Z", "duration": 22},
-        {"name": "The Return of Harmony - Part 2", "time": "2011-09-24T13:00:00.000Z", "duration": 22},
-        {"name": "Lesson Zero", "time": "2011-10-15T13:00:00.000Z", "duration": 22},
-        {"name": "Luna Eclipsed", "time": "2011-10-22T13:00:00.000Z", "duration": 22},
-        {"name": "Sisterhooves Social", "time": "2011-11-05T14:00:00.000Z", "duration": 22},
-        {"name": "The Cutie Pox", "time": "2011-11-12T15:00:00.000Z", "duration": 22},
-        {"name": "May the Best Pet Win!", "time": "2011-11-19T15:00:00.000Z", "duration": 22},
-        {"name": "The Mysterious Mare Do Well", "time": "2011-11-26T15:00:00.000Z", "duration": 22},
-        {"name": "Sweet and Elite", "time": "2011-12-03T15:00:00.000Z", "duration": 22},
-        {"name": "Secret of My Excess", "time": "2011-12-10T15:00:00.000Z", "duration": 22},
-        {"name": "Hearth's Warming Eve", "time": "2011-12-17T15:00:00.000Z", "duration": 22},
-        {"name": "Family Appreciation Day", "time": "2012-01-07T15:00:00.000Z", "duration": 22},
-        {"name": "Baby Cakes", "time": "2012-01-14T15:00:00.000Z", "duration": 22},
-        {"name": "The Last Roundup", "time": "2012-01-21T15:00:00.000Z", "duration": 22},
-        {"name": "The Super Speedy Cider Squeezy 6000", "time": "2012-01-28T15:00:00.000Z", "duration": 22},
-        {"name": "Read It and Weep", "time": "2012-02-04T15:00:00.000Z", "duration": 22},
-        {"name": "Hearts and Hooves Day", "time": "2012-02-11T15:30:00.000Z", "duration": 22},
-        {"name": "A Friend in Deed", "time": "2012-02-18T15:00:00.000Z", "duration": 22},
-        {"name": "Putting Your Hoof Down", "time": "2012-03-03T18:00:00.000Z", "duration": 22},
-        {"name": "It's About Time", "time": "2012-03-10T18:00:00.000Z", "duration": 22},
-        {"name": "Dragon Quest", "time": "2012-03-17T17:00:00.000Z", "duration": 22},
-        {"name": "Hurricane Fluttershy", "time": "2012-03-24T17:00:00.000Z", "duration": 22},
-        {"name": "Ponyville Confidential", "time": "2012-03-31T16:00:00.000Z", "duration": 22},
-        {"name": "MMMystery on the Friendship Express", "time": "2012-04-07T16:00:00.000Z", "duration": 22},
-        {"name": "A Canterlot Wedding - Part 1", "time": "2012-04-21T16:00:00.000Z", "duration": 22},
-        {"name": "A Canterlot Wedding - Part 2", "time": "2012-04-21T16:30:00.000Z", "duration": 22}
+        {"name": "The Return of Harmony - Part 1", "time": "2011-09-17T12:00:00.000Z"},
+        {"name": "The Return of Harmony - Part 2", "time": "2011-09-24T13:00:00.000Z"},
+        {"name": "Lesson Zero", "time": "2011-10-15T13:00:00.000Z"},
+        {"name": "Luna Eclipsed", "time": "2011-10-22T13:00:00.000Z"},
+        {"name": "Sisterhooves Social", "time": "2011-11-05T14:00:00.000Z"},
+        {"name": "The Cutie Pox", "time": "2011-11-12T15:00:00.000Z"},
+        {"name": "May the Best Pet Win!", "time": "2011-11-19T15:00:00.000Z"},
+        {"name": "The Mysterious Mare Do Well", "time": "2011-11-26T15:00:00.000Z"},
+        {"name": "Sweet and Elite", "time": "2011-12-03T15:00:00.000Z"},
+        {"name": "Secret of My Excess", "time": "2011-12-10T15:00:00.000Z"},
+        {"name": "Hearth's Warming Eve", "time": "2011-12-17T15:00:00.000Z"},
+        {"name": "Family Appreciation Day", "time": "2012-01-07T15:00:00.000Z"},
+        {"name": "Baby Cakes", "time": "2012-01-14T15:00:00.000Z"},
+        {"name": "The Last Roundup", "time": "2012-01-21T15:00:00.000Z"},
+        {"name": "The Super Speedy Cider Squeezy 6000", "time": "2012-01-28T15:00:00.000Z"},
+        {"name": "Read It and Weep", "time": "2012-02-04T15:00:00.000Z"},
+        {"name": "Hearts and Hooves Day", "time": "2012-02-11T15:30:00.000Z"},
+        {"name": "A Friend in Deed", "time": "2012-02-18T15:00:00.000Z"},
+        {"name": "Putting Your Hoof Down", "time": "2012-03-03T18:00:00.000Z"},
+        {"name": "It's About Time", "time": "2012-03-10T18:00:00.000Z"},
+        {"name": "Dragon Quest", "time": "2012-03-17T17:00:00.000Z"},
+        {"name": "Hurricane Fluttershy", "time": "2012-03-24T17:00:00.000Z"},
+        {"name": "Ponyville Confidential", "time": "2012-03-31T16:00:00.000Z"},
+        {"name": "MMMystery on the Friendship Express", "time": "2012-04-07T16:00:00.000Z"},
+        {"name": "A Canterlot Wedding - Part 1", "time": "2012-04-21T16:00:00.000Z"},
+        {"name": "A Canterlot Wedding - Part 2", "time": "2012-04-21T16:30:00.000Z"}
     ],
     [
-        {"name": "The Crystal Empire - Part 1", "time": "2012-11-10T15:00:00.000Z", "duration": 22},
-        {"name": "The Crystal Empire - Part 2", "time": "2012-11-10T15:30:00.000Z", "duration": 22},
-        {"name": "Too Many Pinkie Pies", "time": "2012-11-17T15:30:00.000Z", "duration": 22},
-        {"name": "One Bad Apple", "time": "2012-11-24T15:30:00.000Z", "duration": 22},
-        {"name": "Magic Duel", "time": "2012-12-01T15:30:00.000Z", "duration": 22},
-        {"name": "Sleepless in Ponyville", "time": "2012-12-08T15:30:00.000Z", "duration": 22},
-        {"name": "Wonderbolt Academy", "time": "2012-12-15T15:30:00.000Z", "duration": 22},
-        {"name": "Apple Family Reunion", "time": "2012-12-22T15:30:00.000Z", "duration": 22},
-        {"name": "Spike at Your Service", "time": "2012-12-29T15:30:00.000Z", "duration": 22},
-        {"name": "Keep Calm and Flutter On", "time": "2013-01-19T15:30:00.000Z", "duration": 22},
-        {"name": "Just For Sidekicks", "time": "2013-01-26T15:30:00.000Z", "duration": 22},
-        {"name": "Games Ponies Play", "time": "2013-02-09T15:30:00.000Z", "duration": 22},
-        {"name": "Magical Mystery Cure", "time": "2013-02-16T15:30:00.000Z", "duration": 22}
+        {"name": "The Crystal Empire - Part 1", "time": "2012-11-10T15:00:00.000Z"},
+        {"name": "The Crystal Empire - Part 2", "time": "2012-11-10T15:30:00.000Z"},
+        {"name": "Too Many Pinkie Pies", "time": "2012-11-17T15:30:00.000Z"},
+        {"name": "One Bad Apple", "time": "2012-11-24T15:30:00.000Z"},
+        {"name": "Magic Duel", "time": "2012-12-01T15:30:00.000Z"},
+        {"name": "Sleepless in Ponyville", "time": "2012-12-08T15:30:00.000Z"},
+        {"name": "Wonderbolt Academy", "time": "2012-12-15T15:30:00.000Z"},
+        {"name": "Apple Family Reunion", "time": "2012-12-22T15:30:00.000Z"},
+        {"name": "Spike at Your Service", "time": "2012-12-29T15:30:00.000Z"},
+        {"name": "Keep Calm and Flutter On", "time": "2013-01-19T15:30:00.000Z"},
+        {"name": "Just For Sidekicks", "time": "2013-01-26T15:30:00.000Z"},
+        {"name": "Games Ponies Play", "time": "2013-02-09T15:30:00.000Z"},
+        {"name": "Magical Mystery Cure", "time": "2013-02-16T15:30:00.000Z"}
     ],
     [
-        {"name": "Princess Twilight Part 1", "time": "2013-11-23T15:00:00.000Z", "duration": 22},
-        {"name": "Princess Twilight Part 2", "time": "2013-11-23T15:30:00.000Z", "duration": 22},
-        {"name": "Castle Mane-ia", "time": "2013-11-30T15:30:00.000Z", "duration": 22},
-        {"name": "Daring Don't", "time": "2013-12-07T15:30:00.000Z", "duration": 22},
-        {"name": "Flight to the Finish", "time": "2013-12-14T15:30:00.000Z", "duration": 22},
-        {"name": "Power Ponies", "time": "2013-12-21T15:30:00.000Z", "duration": 22},
-        {"name": "Bats!", "time": "2013-12-28T15:30:00.000Z", "duration": 22},
-        {"name": "Rarity Takes Manehattan", "time": "2014-01-04T15:30:00.000Z", "duration": 22},
-        {"name": "Pinkie Apple Pie", "time": "2014-01-11T15:30:00.000Z", "duration": 22},
-        {"name": "Rainbow Falls", "time": "2014-01-18T15:30:00.000Z", "duration": 22},
-        {"name": "Three's a Crowd", "time": "2014-01-25T15:30:00.000Z", "duration": 22},
-        {"name": "Pinkie Pride", "time": "2014-02-01T15:30:00.000Z", "duration": 22},
-        {"name": "Simple Ways", "time": "2014-02-08T15:30:00.000Z", "duration": 22},
-        {"name": "Filli Vanilli", "time": "2014-02-15T15:30:00.000Z", "duration": 22},
-        {"name": "Twilight Time", "time": "2014-02-22T15:30:00.000Z", "duration": 22},
-        {"name": "It Ain't Easy Being Breezies", "time": "2014-03-01T15:30:00.000Z", "duration": 22},
-        {"name": "Somepony to Watch Over Me", "time": "2014-03-08T15:30:00.000Z", "duration": 22},
-        {"name": "Maud Pie", "time": "2014-03-15T14:30:00.000Z", "duration": 22},
-        {"name": "For Whom the Sweetie Belle Toils", "time": "2014-03-22T14:30:00.000Z", "duration": 22},
-        {"name": "Leap of Faith", "time": "2014-03-29T14:30:00.000Z", "duration": 22},
-        {"name": "Testing, Testing, 1, 2, 3", "time": "2014-04-05T14:30:00.000Z", "duration": 22},
-        {"name": "Trade Ya", "time": "2014-04-19T14:30:00.000Z", "duration": 22},
-        {"name": "Inspiration Manifestation", "time": "2014-04-26T14:30:00.000Z", "duration": 22},
-        {"name": "Equestria Games", "time": "2014-05-03T14:30:00.000Z", "duration": 22},
-        {"name": "Twilight's Kingdom - Part 1", "time": "2014-05-10T14:30:00.000Z", "duration": 22},
-        {"name": "Twilight's Kingdom - Part 2", "time": "2014-05-10T15:00:00.000Z", "duration": 22}
+        {"name": "Princess Twilight Part 1", "time": "2013-11-23T15:00:00.000Z"},
+        {"name": "Princess Twilight Part 2", "time": "2013-11-23T15:30:00.000Z"},
+        {"name": "Castle Mane-ia", "time": "2013-11-30T15:30:00.000Z"},
+        {"name": "Daring Don't", "time": "2013-12-07T15:30:00.000Z"},
+        {"name": "Flight to the Finish", "time": "2013-12-14T15:30:00.000Z"},
+        {"name": "Power Ponies", "time": "2013-12-21T15:30:00.000Z"},
+        {"name": "Bats!", "time": "2013-12-28T15:30:00.000Z"},
+        {"name": "Rarity Takes Manehattan", "time": "2014-01-04T15:30:00.000Z"},
+        {"name": "Pinkie Apple Pie", "time": "2014-01-11T15:30:00.000Z"},
+        {"name": "Rainbow Falls", "time": "2014-01-18T15:30:00.000Z"},
+        {"name": "Three's a Crowd", "time": "2014-01-25T15:30:00.000Z"},
+        {"name": "Pinkie Pride", "time": "2014-02-01T15:30:00.000Z"},
+        {"name": "Simple Ways", "time": "2014-02-08T15:30:00.000Z"},
+        {"name": "Filli Vanilli", "time": "2014-02-15T15:30:00.000Z"},
+        {"name": "Twilight Time", "time": "2014-02-22T15:30:00.000Z"},
+        {"name": "It Ain't Easy Being Breezies", "time": "2014-03-01T15:30:00.000Z"},
+        {"name": "Somepony to Watch Over Me", "time": "2014-03-08T15:30:00.000Z"},
+        {"name": "Maud Pie", "time": "2014-03-15T14:30:00.000Z"},
+        {"name": "For Whom the Sweetie Belle Toils", "time": "2014-03-22T14:30:00.000Z"},
+        {"name": "Leap of Faith", "time": "2014-03-29T14:30:00.000Z"},
+        {"name": "Testing, Testing, 1, 2, 3", "time": "2014-04-05T14:30:00.000Z"},
+        {"name": "Trade Ya", "time": "2014-04-19T14:30:00.000Z"},
+        {"name": "Inspiration Manifestation", "time": "2014-04-26T14:30:00.000Z"},
+        {"name": "Equestria Games", "time": "2014-05-03T14:30:00.000Z"},
+        {"name": "Twilight's Kingdom - Part 1", "time": "2014-05-10T14:30:00.000Z"},
+        {"name": "Twilight's Kingdom - Part 2", "time": "2014-05-10T15:00:00.000Z"}
     ]
 ];
 
@@ -105,19 +105,19 @@ episodes.forEach(function(season, si){
     season.forEach(function(episode, ei){
         episode.season = si + 1;
         episode.episode = ei + 1;
+        if(!episode.duration){
+            episode.duration = 22;
+        }
     })
 });
-
 
 exports.all = function(req, res){
     var merged = [];
     res.jsonp(merged.concat.apply(merged, episodes));
 };
 
-
 exports.season = function(req, res){
-    var season = episodes[req.params.season - 1];
-    res.jsonp(season || []);
+    res.jsonp(episodes[req.params.season - 1] || []);
 };
 
 exports.episode = function(req, res){
@@ -131,17 +131,14 @@ exports.until = function(req, res){
     res.jsonp((episode && episode.time) ? [new Date(episode.time) - new Date()] : []);
 };
 
-
 exports.next = function(req, res){
     var merged = [];
     var now = new Date();
     var episode = merged.concat.apply(merged, episodes).filter(function(ep){
         return new Date(ep.time) > now;
     })[0];
-
     res.jsonp(episode ? episode : []);
 };
-
 
 exports.untilNext = function(req, res){
     var merged = [];
@@ -149,6 +146,5 @@ exports.untilNext = function(req, res){
     var episode = merged.concat.apply(merged, episodes).filter(function(ep){
         return new Date(ep.time) > now;
     })[0];
-
     res.jsonp(episode ? [new Date(episode.time) - now] : []);
 };
