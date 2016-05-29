adf107737b1e10ddee1dd7c0262fc303025a02ab
Jim Allanson <jim@amplience.com>
2016-05-29 19:23:21 +0100

    Added S06E11

--- episodes.js
+++ episodes.js
@@ -136,7 +136,8 @@ var episodes = [
         {"name": "Newbie Dash", "time": "2016-05-07T15:30:00.000Z"},
         {"name": "A Hearth's Warming Tail", "time": "2016-05-14T15:30:00.000Z"},
         {"name": "Saddle Row & Rec", "time": "2016-05-21T15:30:00.000Z"},
-        {"name": "Applejack's \"Day\" Off", "time": "2016-05-28T15:30:00.000Z"}
+        {"name": "Applejack's \"Day\" Off", "time": "2016-05-28T15:30:00.000Z"},
+        {"name": "Spice Up Your Life", "time": "2016-06-11T15:30:00.000Z"}
     ]
 ];
 
