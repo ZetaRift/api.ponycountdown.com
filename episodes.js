c65cb985583b25f10effbf959a6e42e64db0ade2
Jim Allanson <jim@amplience.com>
2016-06-11 11:07:42 +0100

    Added future episodes

--- episodes.js
+++ episodes.js
@@ -139,7 +139,12 @@ var episodes = [
         {"name": "Applejack's \"Day\" Off", "time": "2016-05-28T15:30:00.000Z"},
         {"name": "Flutter Brutter", "time": "2016-06-04T15:30:00.000Z"},
         {"name": "Spice Up Your Life", "time": "2016-06-11T15:30:00.000Z"},
-        {"name": "Stranger Then Fan Fiction", "time": "2016-06-18T15:30:00.000Z"}
+        {"name": "Stranger Then Fan Fiction", "time": "2016-06-18T15:30:00.000Z"},
+        {"name": "The Cart Before The Ponies", "time": "2016-06-25T15:30:00.000Z"},
+        {"name": "28 Pranks Later", "time": "2016-07-02T15:30:00.000Z"},
+        {"name": "The Times Are a Changeling", "time": "2016-07-09T15:30:00.000Z"},
+        {"name": "Dungeons And Discords", "time": "2016-07-16T15:30:00.000Z"},
+        {"name": "Buck Ball Season", "time": "2016-07-23T15:30:00.000Z"}
     ]
 ];
 
